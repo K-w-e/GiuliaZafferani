0634812d8e9fb0f79097853643619434175a1d38
K-w-e <alex.guerra988@gmail.com>
2020-03-18 15:24:15 +0100

    Fix

--- js/main.js
+++ js/main.js
@@ -40,11 +40,30 @@ function contact(){
     closeNav();
 }
 
-function show(param_div_id) {
+function show1() {
     document.getElementById('Box').style.display = "";
     document.getElementById('Info').style.display = "none";
     document.getElementById('Contact').style.display = "none";
-    document.getElementById('Box').innerHTML = document.getElementById(param_div_id).innerHTML;
+    var content = "<p class="+"testoprogetto"+">Knowledge must be shared. That is the philosophy from which Fontamara magazine was born. The magazine collects in a box the main themes of the myth of Plato's Cave, from the language creation to the value of culture and critical thinking. There's also a part about a distorted view of the world with themes like the alien invasion or the global cooling and a retrospective of Mostro Graphic Design Camp, where the magazine was exposed.</p>" 
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_01.jpg"+">"
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_02.jpg"+">"
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_03.jpg"+">"
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_04.jpg"+">"
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_05.jpg"+">"
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_06.jpg"+">"
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_07.jpg"+">"
+    +
+    "<img class="+"immagineprogetto"+" src="+"./img/fontamara/Fontamara_08.jpg"+">"
+
+    ;
+    document.getElementById('Box').innerHTML = content;
 
     closeNav();
 }
